ee6a8d988542ede39f32e7ee364bfcdfe94f8334
Mahashree U <162390744+Mahaamimiii@users.noreply.github.com>
2025-09-08 10:52:22 +0545

    Create Day 16 - Min num Alice Bob.java

---
+++ Day 16 - Min num Alice Bob.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] numberGame(int[] nums) {
+        Arrays.sort(nums);
+
+        int[] arr = new int[nums.length];
+
+        int index = 0;
+        for(int i =0; i< nums.length; i+=2){
+            int alice = nums[i];
+            int bob = nums[i+1];
+
+            arr[index++] = bob;
+            arr[index++] = alice;
+
+        }
+        return arr;
+
+
+    }
+}
